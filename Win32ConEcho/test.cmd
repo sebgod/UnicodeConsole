0fa8de9f0190b2f61bedfc28f55fdda037b08431
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-10-02 21:24:44 +0200

    make sure testscript uses the built output

--- Win32ConEcho/test.cmd
+++ Win32ConEcho/test.cmd
@@ -1,10 +1,8 @@
-@echo off
-pushd %~dp0bin\Debug
-@xecho "Test without newline: "
-@xecho -x -n [\e1;32mOK\e0m]
-@xecho -x Test with embedded newline\n
-@xecho -n now adding a newline
-@xecho -x -n "with escaped \" quotes \" "
-@xecho -x -n "Testing \eredmcolor\em-\eMagenta_names\e"
-@xecho -x -n "Colors can be esaped using \\e(^<Name^>//;)[_ m] or the traditional ANSI codes"
-popd
+@set xecho="%~dp0bin\xecho"
+@%xecho% "Test without newline: "
+@%xecho% -x -n [\e1;32mOK\e0m]
+@%xecho% -x Test with embedded newline\n
+@%xecho% -n now adding a newline
+@%xecho% -x -n "with escaped \" quotes \" "
+@%xecho% -x -n "Testing \eredmcolor\em-\eMagenta_names\e"
+@%xecho% -x -n "Colors can be esaped using \\e(^<Name^>//;)[_ m] or the traditional ANSI codes"
