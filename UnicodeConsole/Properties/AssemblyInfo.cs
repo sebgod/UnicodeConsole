ae36806485d0b7db09ac59ef798b22614d271058
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-10-08 11:57:30 +0200

    updated assembly version

--- UnicodeConsole/Properties/AssemblyInfo.cs
+++ UnicodeConsole/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("13.1008.1.1134")]
+[assembly: AssemblyFileVersion("13.1008.1.1134")]
--- Win32ConEcho/Properties/AssemblyInfo.cs
+++ Win32ConEcho/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.1001.28.1004")]
-[assembly: AssemblyFileVersion("13.1001.28.1004")]
+[assembly: AssemblyVersion("13.1008.29.1134")]
+[assembly: AssemblyFileVersion("13.1008.29.1134")]
